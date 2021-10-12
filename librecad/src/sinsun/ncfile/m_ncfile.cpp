7f697e1342d90594db027d76ee8223c739d33885
zmsnfly <zmsnfly@gmail.com>
2021-10-12 16:29:56 +0800

    添加是否加工和蒸发去膜功能

--- librecad/src/sinsun/ncfile/m_ncfile.cpp
+++ librecad/src/sinsun/ncfile/m_ncfile.cpp
@@ -19,15 +19,12 @@ MncFileData MncFileIo::generate_ncFile(const QList<RS_Entity *> &inPutEntities)
     QList<RS_Entity*> entities_ranged = fileFunction.range_entities(inPutEntities, layer_range);
 
     foreach(auto item_entity, entities_ranged)
-    {
+    {      
         MncSolidData tempSolidData;
         tempSolidData.m_is_closed=true;
         tempSolidData.m_is_inner=false;
         tempSolidData.m_part=item_entity;
-        M_PartFunction tem_partFunction = M_PartFunction(tempSolidData);
-        //计算切入点
-        tempSolidData=tem_partFunction.caculate_left_refpoints();
-        m_file.m_segment_file.push_back(tempSolidData);
+
         //处理蒸发去膜
         if(item_entity->m_nc_information.layer_cut>=0)
         {
@@ -36,16 +33,14 @@ MncFileData MncFileIo::generate_ncFile(const QList<RS_Entity *> &inPutEntities)
             ss_LayerCutItem* tem_layerItem = tem_var.value<ss_LayerCutItem*>();
             if(tem_layerItem->getEvaporate())
             {
-                MncSolidData tempSolidData;
-                tempSolidData.m_is_closed=true;
-                tempSolidData.m_is_inner=false;
-                tempSolidData.m_part=item_entity->clone();
-                M_PartFunction tem_partFunction = M_PartFunction(tempSolidData);
-                tempSolidData=tem_partFunction.caculate_left_refpoints();
-                m_file.m_segment_file.push_back(tempSolidData);
+                tempSolidData.m_is_evaporate = true;
             }
         }
 
+        //计算切入点
+        M_PartFunction tem_partFunction = M_PartFunction(tempSolidData);
+        tempSolidData=tem_partFunction.caculate_left_refpoints();
+        m_file.m_segment_file.push_back(tempSolidData);
     }
     return m_file;
 }
@@ -83,9 +78,12 @@ string MncFileIo::generate_gcode()
             g00StartPoint = cutSegments[i-1].getCutOutPoint();
             g00EndPoint = cutSegments[i].getCutInPoint();
         }
-        if(g00StartPoint!=g00EndPoint)
+        if(g00StartPoint == g00EndPoint)
         {
             is_evaporate = true;
+        }
+        else
+        {
             //相对坐标
             if(m_commad==MRELATIVE)
             {
@@ -118,153 +116,153 @@ string MncFileIo::generate_gcode()
         }
 
         //开激光，标记图层信息
-            resultGCode+=gUnit.generate_M(M07, layer);
-
+        resultGCode+=gUnit.generate_M(M07, layer);
         RS_Vector lastRecord=g00EndPoint;
 
         //加工段,这里算上进退刀段一起处理
         M_PartFunction tempProcess(cutSegments[i]);
         vector<shared_ptr<RS_Entity>> keySegments=tempProcess.caculate_data_segmentation();
-
-        for(int j=0;j<keySegments.size();++j)
+        qDebug()<<cutSegments[i].m_is_evaporate+1;
+        for(int index =0; index<(cutSegments[i].m_is_evaporate+1); index++)
         {
-            if(keySegments[j]->rtti()==RS2::EntityCircle)//圆
+            qDebug()<<"执行";
+            for(int j=0;j<keySegments.size();++j)
             {
-                std::string tem_line="";
-                shared_ptr<RS_Circle> tempCircle=dynamic_pointer_cast<RS_Circle>(keySegments[j]);
+                if(keySegments[j]->rtti()==RS2::EntityCircle)//圆
+                {
+                    std::string tem_line="";
+                    shared_ptr<RS_Circle> tempCircle=dynamic_pointer_cast<RS_Circle>(keySegments[j]);
 
-                if(1)//圆默认为顺时针
-                {
-                    //resultGCode+="G02";
-                }
-                else
-                {
-                    //resultGCode+="G03";
-                }
+                    if(1)//圆默认为顺时针
+                    {
+                        //resultGCode+="G02";
+                    }
+                    else
+                    {
+                        //resultGCode+="G03";
+                    }
 
-                RS_Vector tempStartPoint=lastRecord;
-                RS_Vector tempEndPoint=lastRecord;
-                RS_Vector tempCenter=tempCircle->getCenter();
-                std::string _coord="";
-                //相对坐标格式
-                   if(m_commad==MRELATIVE)
-                {
-                    _coord+=("I"+std::to_string(tempCenter.x-tempStartPoint.x)
-                             +" J"+std::to_string(tempCenter.y-tempStartPoint.y));
+                    RS_Vector tempStartPoint=lastRecord;
+                    RS_Vector tempEndPoint=lastRecord;
+                    RS_Vector tempCenter=tempCircle->getCenter();
+                    std::string _coord="";
+                    //相对坐标格式
+                       if(m_commad==MRELATIVE)
+                    {
+                        _coord+=("I"+std::to_string(tempCenter.x-tempStartPoint.x)
+                                 +" J"+std::to_string(tempCenter.y-tempStartPoint.y));
+                    }
+                    //绝对坐标格式
+                    else
+                    {
+                        _coord= "X"+std::to_string(tempEndPoint.x)
+                                   +" Y"+std::to_string(tempEndPoint.y)
+                                   +" I"+std::to_string(tempCenter.x-tempStartPoint.x)
+                                   +" J"+std::to_string(tempCenter.y-tempStartPoint.y);
+                    }
+                    resultGCode+=gUnit.generate_G(G02, _coord);
                 }
-                //绝对坐标格式
-                else
-                {
-                    _coord= "X"+std::to_string(tempEndPoint.x)
-                               +" Y"+std::to_string(tempEndPoint.y)
-                               +" I"+std::to_string(tempCenter.x-tempStartPoint.x)
-                               +" J"+std::to_string(tempCenter.y-tempStartPoint.y);
-                }
-                resultGCode+=gUnit.generate_G(G02, _coord);
-            }
-            else if(keySegments[j]->rtti()==RS2::EntityLine)//直线
-               {
-                   //resultGCode+="G01";
-                   shared_ptr<RS_Line> tempLine=dynamic_pointer_cast<RS_Line>(keySegments[j]);
-                   //相对坐标格式
-                   if(m_commad==MRELATIVE)
+                else if(keySegments[j]->rtti()==RS2::EntityLine)//直线
                    {
-                       RS_Vector g01StartPoint=tempLine->getStartpoint();
-                       RS_Vector g01EndPoint=tempLine->getEndpoint();
-                       std::string deta="";
-                       if(g01StartPoint.x!=g01EndPoint.x && g01StartPoint.y!=g01EndPoint.y)
+                       //resultGCode+="G01";
+                       shared_ptr<RS_Line> tempLine=dynamic_pointer_cast<RS_Line>(keySegments[j]);
+                       //相对坐标格式
+                       if(m_commad==MRELATIVE)
                        {
-                           std::string detaX = "X"+std::to_string(g01EndPoint.x-g01StartPoint.x);
-                           std::string detaY = "Y"+std::to_string(g01EndPoint.y-g01StartPoint.y);
-                           deta = detaX + " " + detaY;
+                           RS_Vector g01StartPoint=tempLine->getStartpoint();
+                           RS_Vector g01EndPoint=tempLine->getEndpoint();
+                           std::string deta="";
+                           if(g01StartPoint.x!=g01EndPoint.x && g01StartPoint.y!=g01EndPoint.y)
+                           {
+                               std::string detaX = "X"+std::to_string(g01EndPoint.x-g01StartPoint.x);
+                               std::string detaY = "Y"+std::to_string(g01EndPoint.y-g01StartPoint.y);
+                               deta = detaX + " " + detaY;
+                           }
+                           else if(g01StartPoint.x!=g01EndPoint.x)
+                           {
+                               std::string detaX = "X"+std::to_string(g01EndPoint.x-g01StartPoint.x);
+                               deta = detaX;
+                           }
+                           else
+                           {
+                               std::string detaY = "Y"+std::to_string(g01EndPoint.y-g01StartPoint.y);
+                               deta = detaY;
+                           }
+
+                           resultGCode+=gUnit.generate_G(G01, deta);
                        }
-                       else if(g01StartPoint.x!=g01EndPoint.x)
+                       //绝对坐标格式
+                       else
                        {
-                           std::string detaX = "X"+std::to_string(g01EndPoint.x-g01StartPoint.x);
-                           deta = detaX;
+                           RS_Vector g01EndPoint=tempLine->getEndpoint();
+                           std::string _coord = "X"+std::to_string(g01EndPoint.x)+" Y"+std::to_string(g01EndPoint.y);
+                           resultGCode+=gUnit.generate_G(G01, _coord);
+                       }
+
+                       lastRecord=tempLine->getEndpoint();
+                   }
+                else if(keySegments[j]->rtti()==RS2::EntityArc)//圆弧
+                   {
+                       shared_ptr<RS_Arc> tempArc=dynamic_pointer_cast<RS_Arc>(keySegments[j]);
+                       G_OPERATE _operate;
+
+                       if(tempArc->isReversed())//顺时针为真
+                       {
+                           _operate = G02;
                        }
                        else
                        {
-                           std::string detaY = "Y"+std::to_string(g01EndPoint.y-g01StartPoint.y);
-                           deta = detaY;
+
+                           _operate = G03;
                        }
 
-                       resultGCode+=gUnit.generate_G(G01, deta);
-                   }
-                   //绝对坐标格式
-                   else
-                   {
-                       RS_Vector g01EndPoint=tempLine->getEndpoint();
-                       std::string _coord = "X"+std::to_string(g01EndPoint.x)+" Y"+std::to_string(g01EndPoint.y);
-                       resultGCode+=gUnit.generate_G(G01, _coord);
-                   }
-
-                   lastRecord=tempLine->getEndpoint();
-               }
-               else if(keySegments[j]->rtti()==RS2::EntityArc)//圆弧
-               {
-                   shared_ptr<RS_Arc> tempArc=dynamic_pointer_cast<RS_Arc>(keySegments[j]);
-                   G_OPERATE _operate;
-
-                   if(tempArc->isReversed())//顺时针为真
-                   {
-                       _operate = G02;
-                   }
-                   else
-                   {
-
-                       _operate = G03;
-                   }
-
-                   RS_Vector tempStartPoint=tempArc->getStartpoint();
-                   RS_Vector tempEndPoint=tempArc->getEndpoint();
-                   RS_Vector tempCenter=tempArc->getCenter();
-                   //相对坐标格式
-                   if(m_commad==MRELATIVE)
-                   {
-                       std::string deta="";
-                       if(tempEndPoint.x!=tempStartPoint.x && tempEndPoint.y!=tempStartPoint.y)
+                       RS_Vector tempStartPoint=tempArc->getStartpoint();
+                       RS_Vector tempEndPoint=tempArc->getEndpoint();
+                       RS_Vector tempCenter=tempArc->getCenter();
+                       //相对坐标格式
+                       if(m_commad==MRELATIVE)
                        {
-                           std::string detaX = "X"+std::to_string(tempEndPoint.x-tempStartPoint.x);
-                           std::string detaY = "Y"+std::to_string(tempEndPoint.y-tempStartPoint.y);
-                           deta = detaX + " " + detaY;
+                           std::string deta="";
+                           if(tempEndPoint.x!=tempStartPoint.x && tempEndPoint.y!=tempStartPoint.y)
+                           {
+                               std::string detaX = "X"+std::to_string(tempEndPoint.x-tempStartPoint.x);
+                               std::string detaY = "Y"+std::to_string(tempEndPoint.y-tempStartPoint.y);
+                               deta = detaX + " " + detaY;
+                           }
+                           else if(tempEndPoint.x!=tempStartPoint.x)
+                           {
+                               std::string detaX = "X"+std::to_string(tempEndPoint.x-tempStartPoint.x);
+                               deta = detaX;
+                           }
+                           else
+                           {
+                               std::string detaY = "Y"+std::to_string(tempEndPoint.y-tempStartPoint.y);
+                               deta = detaY;
+                           }
+                           deta += " I"+std::to_string(tempCenter.x-tempStartPoint.x)+" J"+std::to_string(tempCenter.y-tempStartPoint.y);
+                           resultGCode+=gUnit.generate_G(_operate, deta);
                        }
-                       else if(tempEndPoint.x!=tempStartPoint.x)
-                       {
-                           std::string detaX = "X"+std::to_string(tempEndPoint.x-tempStartPoint.x);
-                           deta = detaX;
-                       }
+                       //绝对坐标格式
                        else
                        {
-                           std::string detaY = "Y"+std::to_string(tempEndPoint.y-tempStartPoint.y);
-                           deta = detaY;
+                           std::string _coord = "";
+                           _coord = "X"+std::to_string(tempEndPoint.x)
+                                   +" Y"+std::to_string(tempEndPoint.y)
+                                   +" I"+std::to_string(tempCenter.x-tempStartPoint.x)
+                                   +" J"+std::to_string(tempCenter.y-tempStartPoint.y);
+                           resultGCode+=gUnit.generate_G(_operate, _coord);
                        }
-                       deta += " I"+std::to_string(tempCenter.x-tempStartPoint.x)+" J"+std::to_string(tempCenter.y-tempStartPoint.y);
-                       resultGCode+=gUnit.generate_G(_operate, deta);
+                       lastRecord=tempArc->getEndpoint();
                    }
-                   //绝对坐标格式
-                   else
-                   {
-                       std::string _coord = "";
-                       _coord = "X"+std::to_string(tempEndPoint.x)
-                               +" Y"+std::to_string(tempEndPoint.y)
-                               +" I"+std::to_string(tempCenter.x-tempStartPoint.x)
-                               +" J"+std::to_string(tempCenter.y-tempStartPoint.y);
-                       resultGCode+=gUnit.generate_G(_operate, _coord);
-                   }
-                   lastRecord=tempArc->getEndpoint();
-               }
-            else if(keySegments[j]->rtti()==RS2::EntityPoint)
-            {
+                else if(keySegments[j]->rtti()==RS2::EntityPoint)
+                {
 
+                }
             }
         }
+
         //关激光
-        if(!is_evaporate)
-        {
-           resultGCode+=gUnit.generate_M(M08);
-        }
-
+        resultGCode+=gUnit.generate_M(M08);
     }
     return resultGCode;
 }
